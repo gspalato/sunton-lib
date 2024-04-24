81e7fed25a01b7b0fecd7e7e51d4acac32102c00
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-04-24 20:38:11 -0300

    I'm honestly getting tired of trying to find the sweet spot.

--- src/esp32_smartdisplay.c
+++ src/esp32_smartdisplay.c
@@ -214,9 +214,8 @@ void smartdisplay_init()
   disp_drv.draw_buf = (lv_disp_draw_buf_t *)malloc(sizeof(lv_disp_draw_buf_t));
 
   void *drawBuffer = heap_caps_malloc(sizeof(lv_color_t) * LVGL_BUFFER_PIXELS * 2, LVGL_BUFFER_MALLOC_FLAGS);
-  void *drawBuffer2 = heap_caps_malloc(sizeof(lv_color_t) * LVGL_BUFFER_PIXELS * 2, LVGL_BUFFER_MALLOC_FLAGS);
 
-  lv_disp_draw_buf_init(disp_drv.draw_buf, drawBuffer, drawBuffer2, LVGL_BUFFER_PIXELS * 2);
+  lv_disp_draw_buf_init(disp_drv.draw_buf, drawBuffer, NULL, LVGL_BUFFER_PIXELS * 2);
 
   // Register callback for changes to the driver parameters (rotation!)
   disp_drv.drv_update_cb = lvgl_update_callback;
